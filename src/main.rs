68cec21d7202fedb8b296d3c95c0d4e582f6bce1
bushmrt1 <randy.bushman@jhuapl.edu>
2025-07-11 11:37:21 -0400

    Use structs to run experiments

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,16 @@
 extern crate rand;
 mod sorting;
 mod utils;
-
 use crate::sorting::{counting_sort, merge_sort, qr_sort, quicksort, radix_sort};
 use crate::utils::{is_sorted, linspace};
-use clap::Parser;
 use itertools::Itertools;
 use rand::SeedableRng;
 use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
-use std::fmt::{Write, format};
+use std::fmt::Write;
 use std::fs::File;
 use std::io::Write as IOWrite;
 use std::time::Instant;
-
-use std::fs::OpenOptions;
-use std::path::Path;
-
-/// Program to compare performance of sorting algorithms
-
-#[derive(Parser)]
-#[command(name = "sort_eval", about = "")]
-struct Args {
-    /// Number of sorting trials to execute
-    #[arg(short = 't', default_value_t = 5)]
-    trials: usize,
-
-    /// The size of the smallest array to sort
-    #[arg(short = 's', default_value_t = 10000)]
-    start_length: usize,
-
-    /// The size of the largest array to sort
-    #[arg(short = 'e', default_value_t = 1000000)]
-    end_length: usize,
-
-    /// The length in which an array increments after each trial period
-    #[arg(short = 'i', default_value_t = 10000)]
-    length_increment: usize,
-
-    /// The minimum possible array value to sort
-    #[arg(short = 'm', default_value_t = 0)]
-    min_value: i64,
-
-    ///
-    #[arg(short = 'M', long, value_delimiter = ',', value_parser = clap::value_parser!(i64), default_value = "1000000000000")]
-    max_values: Vec<i64>,
-
-    #[arg(short = 'o', default_value_t = String::from("./results"))]
-    output_dir: String,
-}
 
 struct SortingBuffers {
     aux_buffer: Vec<i64>,
@@ -56,139 +18,148 @@ struct SortingBuffers {
     counting_buffer: Vec<usize>,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    // Check if the output
-    let output_dir = args.output_dir;
-    assert!(
-        !filepath_allows_write(&output_dir),
-        "Output filepath is invalid or not writable."
-    );
-
-    // Parse command line arguments
-    let trial_count = args.trials;
-    let start_length = args.start_length;
-    let end_length = args.end_length;
-    let length_increment = args.length_increment;
-    let min_value = args.min_value;
-    let max_values = args.max_values;
-
-    // Select seed to get reproducible results
-    let seed: u64 = 42; // choose your seed
-    let mut rng = StdRng::seed_from_u64(seed);
-
-    // A list of algorithms to evaluate
-    let algorithms = [
-        //run_merge_sort,
-        //run_quicksort,
-        //run_counting_sort,
-        run_radix_sort,
-        run_qr_sort,
-    ];
-
-    // The accompanying names of the algorithms defined above
-    let algorithm_names = [
-        //"Merge Sort",
-        //"Quicksort",
-        //"Counting Sort",
-        "Radix Sort",
-        "QR Sort",
-    ];
-
-    // An array that tracks each algorithm's execution time
-    let mut algorithm_times: Vec<u128> = vec![0; algorithms.len()];
-
-    let max_range_value = *max_values.iter().max().expect("Error parsing max values.") - min_value;
-    let mut sorting_buffers = SortingBuffers {
-        aux_buffer: vec![0; end_length],
-        keys_buffer: vec![0; end_length],
-        counting_buffer: vec![0; ((max_range_value + 1) as usize).max(end_length)],
-    };
-    
-    // Iterate through the given max values (users can input many max values that make a new experiment)
-    for max_value in max_values.iter() {
-        let range = (*max_value - min_value) as usize;
-
-        println!("Begin experiment with range {min_value}-{max_value}");
-
-        // A string that captures all the experiment headers information
-        let mut experiment_text: String = std::iter::once("Length")
-            .chain(algorithm_names.iter().copied())
-            .join(",")
-            + "\n";
-
-        // From start length to end length perform trials to evaluate algorithm performance
-        for arr_len in (start_length..=end_length).step_by(length_increment) {
-            println!("Begin trials with length {arr_len}");
-
-            // Initialize a linearly spaced array and a copy
-            let mut base_arr = linspace(min_value, *max_value, arr_len);
-            let mut arr_copy = vec![0; arr_len];
-
-            // Each experiment executes `trial_count` trials
-            // Each algorithm sorts the same copy of the base array
-            // The base array is shuffled for every trial
-            for _ in 0..trial_count {
-                base_arr.shuffle(&mut rng);
-
-                // Iterate though each algorithm, execute it, and capture its runtime
-                for (i, sort_fn) in algorithms.iter().enumerate() {
-                    // Copy the base array so each algorithm sorts the same sequences
-                    arr_copy.clone_from(&base_arr);
-
-                    // Execute the sorting algorithm and track the time (ms) it takes
-                    let start = Instant::now();
-                    sort_fn(&mut arr_copy, &mut sorting_buffers,  range);
-                    algorithm_times[i] += start.elapsed().as_micros();
-
-                    //if !is_sorted(&arr_copy) {
-                    //    eprintln!("Failed to sort array using {}.", algorithm_names[i]);
-                    //}
-                }
-            }
-
-            // Write the array length to the experiments string
-            write!(&mut experiment_text, "{arr_len}")
-                .expect("Could not write array length to experiment string");
-
-            // Compute the averages of each algorithm time and reset the array
-            let averages = algorithm_times
-                .iter_mut()
-                .map(|time| {
-                    let average = *time / (trial_count as u128);
-                    *time = 0;
-                    format!(",{average}")
-                })
-                .join("");
-
-            // Write the averages to the experiment_text
-            write!(experiment_text, "{averages}\n").expect("Could not averages write to string");
+impl SortingBuffers {
+    fn new_from_experiment(experiment: &SortingExperiment) -> Self {
+        let max_range_value = experiment
+            .max_values
+            .iter()
+            .max()
+            .expect("Error parsing max values.")
+            - experiment.min_value;
+        Self {
+            aux_buffer: vec![0; experiment.end_length],
+            keys_buffer: vec![0; experiment.end_length],
+            counting_buffer: vec![0; ((max_range_value + 1) as usize).max(experiment.end_length)],
         }
-
-        // After completing an experiment by length, write the results to a file
-        let filename = format!("{output_dir}/{min_value}min_value-{max_value}max_value.csv");
-        write_string_to_file(&filename, &experiment_text);
-
-        println!("Done with experiment {filename}");
-
-        // Clear for next experiment
-        experiment_text.clear()
     }
 }
 
-/// Checks write permission and validity for a file path.
-/// :return: true if writable, false otherwise
-fn filepath_allows_write(dir_path: &str) -> bool {
-    OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(Path::new(dir_path))
-        .is_ok()
+struct SortingExperiment {
+    trials: usize,
+    start_length: usize,
+    end_length: usize,
+    length_inc: usize,
+    min_value: i64,
+    max_values: Vec<i64>,
+    output_dir: &'static str,
+    algorithm_name_headers: Vec<&'static str>,
+    algorithm_functions: Vec<fn (arr: &mut [i64], sorting_buffers: &mut SortingBuffers, range: usize) -> u128>,
+    random_seed_rng: StdRng,
+}
+
+impl SortingExperiment {
+    fn new_all_experiment() -> Self {
+        Self {
+            trials:         5,
+            start_length:   10_000,
+            end_length:     1_000_000,
+            length_inc:     10_000,
+            min_value:      0,
+            max_values: (4..8).map(|exp| 10i64.pow(exp)).collect(),
+            output_dir: "./results_all",
+            algorithm_name_headers: vec!["Quicksort", "Merge Sort", "Counting Sort", "Radix Sort", "QR Sort"],
+            algorithm_functions: vec![run_quicksort, run_merge_sort, run_counting_sort, run_radix_sort, run_qr_sort],
+            random_seed_rng: StdRng::seed_from_u64(42),
+        }
+    }
+
+    fn new_only_radix_sort_and_qr_sort() -> Self {
+        Self {
+            trials:         5,
+            start_length:   10_000,
+            end_length:     1_000_000,
+            length_inc:     10_000,
+            min_value:      0,
+            max_values: (4..=12).map(|exp| 10i64.pow(exp)).collect(),
+            output_dir: "./results_radix_qr",
+            algorithm_name_headers: vec!["Radix Sort", "QR Sort"],
+            algorithm_functions: vec![run_radix_sort, run_qr_sort],
+            random_seed_rng: StdRng::seed_from_u64(42),
+        }
+    }
+
+    fn run_experiment(self: &mut Self) {
+        // Tallies the time it takes in Microseconds each algorithm takes
+        let mut algorithm_times: Vec<u128> = vec![0; self.algorithm_functions.len()];
+
+        // Initializes mutable buffers that can be used by each sorting algorithm
+        let mut sorting_buffers = SortingBuffers::new_from_experiment(&self);
+
+        // Iterate through each of the max values defined
+        for max_value in self.max_values.iter() {
+
+            // Compute the range of the array to be sorted 
+            let range = (*max_value - self.min_value) as usize;
+            println!("Begin experiment with range {}-{max_value}", self.min_value);
+
+            // A string that captures all the experiment headers information
+            let mut header_text: String = std::iter::once("Length")
+                .chain(self.algorithm_name_headers.iter().copied())
+                .join(",")
+                + "\n";
+
+            // From start length to end length perform trials to evaluate algorithm performance
+            for arr_len in (self.start_length..=self.end_length).step_by(self.length_inc) {
+                println!("Begin trials with length {arr_len}");
+
+                // Initialize a linearly spaced array and a copy
+                let mut base_arr = linspace(self.min_value, *max_value, arr_len);
+                let mut arr_copy = vec![0; arr_len];
+
+                // Each experiment executes `trials` number of trials
+                for _ in 0..self.trials {
+                    base_arr.shuffle(&mut self.random_seed_rng);
+
+                    // Iterate though each algorithm function, execute it, and tally its runtime
+                    for (i, sort_fn) in self.algorithm_functions.iter().enumerate() {
+                        // Copy the base array so each algorithm sorts the same sequences
+                        arr_copy.clone_from(&base_arr);
+
+                        // Execute the sorting algorithm and track the time (ms) it takes
+                        let start = Instant::now();
+                        sort_fn(&mut arr_copy, &mut sorting_buffers, range);
+                        algorithm_times[i] += start.elapsed().as_micros();
+
+                        //if !is_sorted(&arr_copy) {
+                        //    eprintln!("Failed to sort array using {}.", algorithm_names[i]);
+                        //}
+                    }
+                }
+
+                // Write the array length to the experiments string
+                write!(&mut header_text, "{arr_len}")
+                    .expect("Could not write array length to experiment string");
+
+                // Compute the averages of each algorithm time and reset the array
+                let averages = algorithm_times
+                    .iter_mut()
+                    .map(|time| {
+                        let average = *time / (self.trials as u128);
+                        *time = 0;
+                        format!(",{average}")
+                    })
+                    .join("");
+
+                // Write the averages to the experiment_text
+                write!(header_text, "{averages}\n")
+                    .expect("Could not averages write to string");
+            }
+
+            // After completing an experiment by length, write the results to a file
+            let filename = format!("{}/{}min_value-{max_value}max_value.csv", self.output_dir, self.min_value);
+            write_string_to_file(&filename, &header_text);
+
+            println!("Done with experiment {filename}");
+
+            // Clear for next experiment
+            header_text.clear()
+        }
+    }
 }
 
 fn write_string_to_file(filename: &str, data: &String) {
-    let mut file = File::create(filename).expect(&format!("Could not open file: {filename}"));
+    let mut file = File::create(filename)
+        .expect(&format!("Could not open file: {filename}"));
     file.write_all(data.as_bytes()).unwrap();
 }
 
@@ -228,7 +199,7 @@ fn run_counting_sort(arr: &mut [i64], sorting_buffers: &mut SortingBuffers, rang
     // Reset the buffer slices
     aux_arr_buffer.fill(0);
     counting_buffer.fill(0);
-    
+
     elapsed
 }
 
@@ -256,16 +227,21 @@ fn run_radix_sort(arr: &mut [i64], sorting_buffers: &mut SortingBuffers, range: 
     let aux_arr_buffer = &mut sorting_buffers.aux_buffer[..arr.len()];
     let key_buffer = &mut sorting_buffers.keys_buffer[..arr.len()];
     let counting_buffer = &mut sorting_buffers.counting_buffer[..arr.len()];
-    
+
     // Track the time for Radix Sort to run
     let time_tracker = Instant::now();
     radix_sort(arr, aux_arr_buffer, key_buffer, counting_buffer);
     let elapsed = time_tracker.elapsed().as_micros();
-    
+
     // Reset the buffer slices
     aux_arr_buffer.fill(0);
     key_buffer.fill(0);
     counting_buffer.fill(0);
-    
+
     elapsed
+}
+
+fn main() {
+    SortingExperiment::new_only_radix_sort_and_qr_sort().run_experiment();
+    //SortingExperiment::new_all_experiment().run_experiment();
 }
